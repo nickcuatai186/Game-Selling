b2f9df1672690c702e23c2c2fc258697492005e2
FaradayJr <ntt18602@gmail.com>
2023-06-06 23:58:53 +0700

    Create ThemHinh_CTSP.aspx.cs

---
+++ Admin/ThemHinh_CTSP.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+namespace DoanWeb.Admin
+{
+    public partial class ThemHinh_CTSP : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnthem_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Web_BH.mdf;Integrated Security=True;Connect Timeout=30");
+            string insertStr = "INSERT INTO Hinh_CTSP VALUES (@MaSP, @Hinhanh,@Ghichu)";
+            SqlCommand cmd = new SqlCommand(insertStr, con);
+            cmd.Parameters.AddWithValue("@MaSP", txtmasp.Text);
+            cmd.Parameters.AddWithValue("@Hinhanh", txthinhanh.Text);
+            cmd.Parameters.AddWithValue("@Ghichu", txtghichu.Text);
+            using (con)
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+            }
+            txtmasp.Text = null;
+            txthinhanh.Text = null;
+            txtghichu.Text = null;
+            txtmasp.Focus();
+        }
+    }
+}
